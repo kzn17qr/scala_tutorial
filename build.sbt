8cb5d80ca6f1f5b5d2452275a4c3bdc5b6612143
kzn17qr <kzn17qr@gmail.com>
2016-01-11 02:02:18 +0000

    Finished Step 3

--- build.sbt
+++ build.sbt
@@ -6,3 +6,4 @@ scalaVersion := "2.11.7"
 
 scalaJSStage in Global := FastOptStage
 
+libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.8.0"
--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -1,9 +1,19 @@
 package tutorial.webapp
 
 import scala.scalajs.js.JSApp
+import org.scalajs.dom
+import dom.document
 
 object TutorialApp extends JSApp {
+  def appendPar(targetNode: dom.Node, text: String): Unit= {
+    val parNode = document.createElement("p")
+    val textNode = document.createTextNode(text)
+    parNode.appendChild(textNode)
+    targetNode.appendChild(parNode)
+  }
+
   def main(): Unit = {
-    println("Hello world...!")
+    appendPar(document.body, "Hello world.")
+    appendPar(document.body, "This is one of test pages.")
   }
 }
